cd4ec71e6709d14fb5c8faef87f585b03690a530
loo <loo@loolab.org>
2011-12-08 02:55:32 +0100

    Work on view continues...

--- modules/view/view.class.php
+++ modules/view/view.class.php
@@ -1,10 +1,18 @@
 <?php
-
-// require_once 'mysfw_core.class.php';
-// require_once 'mysfw_view.interface.php';
+ // XXX WIP
 
  class mysfw_view extends mysfw_core implements mysfw_view_interface {
   protected $_home = ''; // XXX
+
+  public function get_ready() {
+   if(($c = $this->get_configurator()) && ($dir = $c->inform('tmpl_root'))){
+    $this->_home = $dir;
+    return $this;
+   }
+
+   $this->_home = 'tmpl/'; // XXX temp
+   return $this;
+  }
 
   public function get($k) {return $this->_values[$k];}
   public function set($k, $v) {$this->_values[$k] = $v;}
