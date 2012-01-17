e2d36c629e3269a57573c03c7ed70c077760e4df
loo <loo@loolab.org>
2012-01-17 21:45:34 +0100

    The underlaying view used by controller_base is now externally configurable, via the configurator object

--- modules/controller_base/mysfw_controller_base.class.php
+++ modules/controller_base/mysfw_controller_base.class.php
@@ -3,6 +3,9 @@
  class mysfw_controller_base extends mysfw_core implements mysfw_controller, mysfw_dna {
   private $_v;    // Object implementing mysfw_view
   private $_tmpl; // Template to be used by the view
+  protected $_defaults = array(
+    'controller_base.view' => 'view' // Name of the view to be used by the controller
+    );
 
   protected function _set_tmpl($_){$this->_tmpl = $_;}
   protected function _get_tmpl(){return $this->_tmpl;}
@@ -11,7 +14,7 @@
   protected function _get($_){return $this->_v->get($_);}
 
   protected function _get_ready() {
-   $this->_v = $this->get_popper()->pop('view'); // XXX temp
+   $this->_v = $this->get_popper()->pop($this->inform('controller_base.view'));
   }
 
   public function control_and_reveal($p) {
