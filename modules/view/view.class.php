bba171ac6fa006f544136306c021a9cc9e505d1a
Laurent Grimaud <laurent.grimaud@gmail.com>
2012-02-16 10:20:00 +0100

    Temporary mechanism to bring http status code to response object

--- modules/view/view.class.php
+++ modules/view/view.class.php
@@ -14,6 +14,10 @@
   public function get_all() {return $this->_values;}
 
   public function reveal($t) {
+   // XXX loo temp: should use another mechanism but configurator to bring http status code to response object
+   if($c = $this->get('status_code')){
+    $this->define('response.http_status_code', $c);
+   }
    $this->get_popper()->pop($this->inform("view.response"))->reveal();
    include $this->inform('root').$this->inform('tmpl_dir').$t.'.tmpl.php'; // XXX 
   }
