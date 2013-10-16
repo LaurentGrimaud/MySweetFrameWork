c9dcc1ade777dbb9e71ebbbe4b6bfb4c1cdd309f
Laurent Grimaud <laurent.grimaud@gmail.com>
2013-10-16 16:20:32 +0200

    All setters return the current object

--- modules/controller_base/mysfw_controller_base.class.php
+++ modules/controller_base/mysfw_controller_base.class.php
@@ -7,13 +7,13 @@
     'controller_base.view' => 'http_response' // Name of the view to be used by the controller
     );
 
-  protected function _set_tmpl($_){$this->_tmpl = $_;}
+  protected function _set_tmpl($_){$this->_tmpl = $_;return $this;}
   protected function _get_tmpl(){return $this->_tmpl;}
 
-  protected function _set($k, $v){$this->_v->set($k, $v);}
+  protected function _set($k, $v){$this->_v->set($k, $v);return $this;}
   protected function _get($_){return $this->_v->get($_);}
   
-  protected function _set_all($_){$this->_v->set_all($_);}
+  protected function _set_all($_){$this->_v->set_all($_);return $this;}
 
   // XXX _get_ready() may be overriden by custom controllers
   protected function _get_ready() {
