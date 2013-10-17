efaeb2129daf12ad3ec9c3a8283153ccfdbbe142
Laurent Grimaud <laurent.grimaud@gmail.com>
2013-10-17 06:46:43 +0200

    Deletion of obsolete files

--- module/configurator/default_configurator.module.php
+++
@@ -1,6 +0,0 @@
-<?php
- $this->_learn("substructure/mysfw_core.class.php");
- $this->_learn("substructure/mysfw_configurator.interface.php");
- $this->_learn("modules/default_configurator/mysfw_default_configurator.class.php");
-
-?>
