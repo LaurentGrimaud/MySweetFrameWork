3543a4098913077b01d319119c5d33c7df69eba4
Laurent Grimaud <laurent.grimaud@gmail.com>
2013-10-17 07:39:59 +0200

    Renaming, namespacing and relocating

---
+++ module/dispatcher/dispatcher.php
@@ -0,0 +1,32 @@
+<?php
+ //XXX no contract for this class ?
+
+ namespace t0t1\mysfw\module\dispatcher;
+ use t0t1\mysfw\frame;
+
+ class dispatcher extends frame\dna implements frame\contract\dna {
+  protected $_defaults = [
+   'controller_parameter' => 'controller',
+   'controller_suffix'    => '_controller',
+   'controller_default'   => 'index',
+   ];
+
+  protected function _get_ready() {
+   $this->report_debug("Dispatcher is ready");
+  }
+
+  /**
+   * XXX params in configurator ?
+   * XXX params object or array ?
+   */
+  public function dispatch($params) {
+   if($controller = @$params[$this->inform('controller_parameter')]){
+    $this->report_debug("Found specified controller `$controller`");
+   }else{
+    $controller = $this->inform('controller_default');
+    $this->report_debug("No specified controller, will use default `{$this->inform('controller_default')}` one");
+   }
+
+   $this->get_popper()->pop($controller.$this->inform('controller_suffix'))->control_and_reveal((object)$params);
+  }
+ }
