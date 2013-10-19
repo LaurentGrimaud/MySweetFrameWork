6f4ba2bf2f4e271513fd785ae97edc788f4ef58d
Laurent Grimaud <laurent.grimaud@gmail.com>
2013-10-19 03:37:01 +0200

    Code aesthetic...

--- modules/cached_data_storage/cached_data_storage.module.php
+++ modules/cached_data_storage/cached_data_storage.module.php
@@ -2,4 +2,3 @@
  $this->_learn("substructure/mysfw_core.class.php");
  $this->_learn("substructure/mysfw_data_storage.interface.php");
  $this->_learn("modules/cached_data_storage/cached_data_storage.class.php");
-?>
