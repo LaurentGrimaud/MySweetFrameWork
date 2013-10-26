a280c5e36ca45c1c3ed64499dbddc92d66c8d736
Laurent Grimaud <laurent.grimaud@gmail.com>
2013-10-26 03:29:25 +0200

    the dispatcher now uses generic_controller

--- module/dispatcher/dispatcher.php
+++ module/dispatcher/dispatcher.php
@@ -6,9 +6,9 @@
 
  class dispatcher extends frame\dna implements frame\contract\dna {
   protected $_defaults = [
-   'controller_parameter' => 'controller',
-   'controller_suffix'    => '_controller',
-   'controller_default'   => 'index',
+   'dispatcher:controller' => 'generic_controller',
+   'dispatcher:parameter'  => 'controller',
+   'dispatcher:default'    => 'index',
    ];
 
   protected function _get_ready() {
@@ -20,13 +20,13 @@
    * XXX params object or array ?
    */
   public function dispatch($params) {
-   if($controller = @$params[$this->inform('controller_parameter')]){
+   if($controller = @$params[$this->inform('dispatcher:parameter')]){
     $this->report_debug("Found specified controller `$controller`");
    }else{
-    $controller = $this->inform('controller_default');
-    $this->report_debug("No specified controller, will use default `{$this->inform('controller_default')}` one");
+    $controller = $this->inform('dispatcher:default');
+    $this->report_debug("No specified controller, will use default `{$this->inform('dispatcher:default')}` one");
    }
 
-   $this->get_popper()->pop($controller.$this->inform('controller_suffix'))->control_and_reveal((object)$params);
+   $this->get_popper()->pop($this->inform('dispatcher:controller'))->set_param($controller)->control_and_reveal((object)$params);
   }
  }
