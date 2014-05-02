bf0e2d5e5f32fa0ca9f7888b0ec6dd2db3fc3e49
g4rmr <romain.m.giacalone@gmail.com>
2014-05-02 11:07:00 +0200

    Added filter concept

---
+++ module/filter/filter.php
@@ -0,0 +1,25 @@
+<?php namespace t0t1\mysfw\module;
+use t0t1\mysfw;
+
+class filter extends mysfw\frame\dna{
+
+    public function apply(
+        $value,
+        array $filters= array()
+    ){
+        if( is_callable($value)) $value = call_user_func($value);
+        if( is_array($value)) return $value; //XXX recursively validate array
+        foreach($filters as $filter){
+            if(is_callable($filter)) $value = call_user_func($filter,$value);
+        }
+        return $value;
+    }
+
+    public function filter_email( $email){
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public function filter_string( $string){
+        return filter_var($string, FILTER_SANITIZE_STRING);
+    }
+}
