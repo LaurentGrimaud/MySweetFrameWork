93e46e44846776807f44c44b0da167fc50d13194
g4rmr <romain.m.giacalone@gmail.com>
2014-05-28 17:58:47 +0200

    redirecting now always send http status code 301

--- module/router/router.php
+++ module/router/router.php
@@ -8,6 +8,8 @@ class router extends mysfw\frame\dna{
     ){
         $url = ($route instanceof route)?$route->build_url():$route;
         $this->report_info('redirecting to ' . $url);
-        exit($this->get_popper()->pop('http_response')->set_http_response_header('Location',$url)->reveal());
+        $response = $this->get_popper()->pop('http_response');
+        $response->set('status_code','301');
+        exit($response->set_http_response_header('Location',$url)->reveal());
     }
 }
