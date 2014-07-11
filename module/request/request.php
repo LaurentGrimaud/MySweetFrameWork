33d8adea0cd24306ae02e10a9ee5f48b9b5fc8b0
g4rmr <romain.m.giacalone@gmail.com>
2014-07-11 09:40:00 +0200

    modified request object to include filters

--- module/request/request.php
+++ module/request/request.php
@@ -8,6 +8,8 @@ class request extends mysfw\frame\dna{
     protected $_server= array();
     protected $_files= array();
 
+    protected $_filter= null;
+
     protected $_defaults= array(
         'INPUT_GET'=> array(),
         'INPUT_POST'=> array(),
@@ -20,33 +22,50 @@ class request extends mysfw\frame\dna{
         $this->_post = $this->inform('request:INPUT_POST')?:$_POST;
         $this->_server = $this->inform('request:INPUT_SERVER')?:$_SERVER;
         $this->_files = $this->inform('request:INPUT_FILES')?:$_FILES;
+        $this->_filter = $this->get_popper()->pop('filter');
     }
-    public function get_query($k=null) {
+    public function get_query($k=null, array $filters=null) {
         if(empty($k)  and $k!==0) return $this->_query;
-        if(isset($this->_query[$k])) return $this->_query[$k];
+        if(isset($this->_query[$k])){
+            if( $filters) return $this->_filter->apply($this->_query[$k],$filters);
+            return $this->_query[$k];
+        }
         return null;
     }
 
-    public function get_post($k=null) {
+    public function get_post($k=null, array $filters=null) {
         if(empty($k)  and $k!==0) return $this->_post;
-        if(isset($this->_post[$k])) return $this->_post[$k];
+        if(isset($this->_post[$k])){
+            if( $filters) return $this->_filter->apply($this->_post[$k],$filters);
+            return $this->_post[$k];
+        }
         return null;
     }
 
-    public function get_server($k=null) {
+    public function get_server($k=null, array $filters=null) {
         if(empty($k)  and $k!==0) return $this->_server;
-        if(isset($this->_server[$k])) return $this->_server[$k];
+        if(isset($this->_server[$k])){
+            if( $filters) return $this->_filter->apply($this->_server[$k],$filters);
+            return $this->_server[$k];
+        }
         return null;
     }
 
-    public function get_files($k=null) {
+    public function get_files($k=null, array $filters=null) {
         if(empty($k)  and $k!==0) return $this->_files;
-        if(isset($this->_files[$k])) return $this->_files[$k];
+        if(isset($this->_files[$k])){
+            if( $filters) return $this->_filter->apply($this->_files[$k],$filters);
+            return $this->_files[$k];
+        }
         return null;
     }
 
     public function is_post(){
-        return ($this->get_server('REQUEST_METHOD')=='POST');
+        return ($this->_filter->apply($this->get_server('REQUEST_METHOD'),array('trim'))=='POST');
+    }
+
+    public function accepts_json(){
+        return ( false !== strpos($this->_filter->apply($this->get_server('HTTP_ACCEPT'),array('trim')),'application/json'));
     }
     public function allows_cookies(){
         $use_cookie = ini_get('session.use_cookies');
