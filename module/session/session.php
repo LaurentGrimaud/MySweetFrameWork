6a87363be1f79183a684e0c6a7d4d4b8f26f6662
g4rmr <romain.m.giacalone@gmail.com>
2014-08-29 09:22:29 +0200

    session module phpunit test class

--- module/session/session.php
+++ module/session/session.php
@@ -5,21 +5,17 @@ class session extends mysfw\frame\dna{
 
     public function start(){
         if( ! $this->is_active()){
-            if( ! session_start()){
+            if( ! @session_start()){
                 $this->report_error('Session failed to start');
                 return false;
             }
-        }
+            $this->report_debug('Session has started');
+        } else $this->report_debug('Session already started');
         return $this;
     }
 
     public function destroy(){
-        if ( ! $this->is_active() ){ // start session if not started yet
-            if( ! session_start()){
-                $this->report_error('Session failed to start');
-                return false;
-            }
-        }
+        $this->start();
         session_unset();
         $_SESSION= array();
         if( ! session_destroy()){
@@ -31,11 +27,12 @@ class session extends mysfw\frame\dna{
             $params['name']= session_name();
             $params['time']= time()-42000;
             $params['value']= "";
-            if( ! setcookie($params['name'], $params['value'], $params['time'],@$params['path'], @$params['domain'], @$params['secure'], @$params['httponly'])){ //XXX should use some method belonging to a cookie object to destroy session's cookie
+            if( ! @setcookie($params['name'], $params['value'], $params['time'],@$params['path'], @$params['domain'], @$params['secure'], @$params['httponly'])){ //XXX should use some method belonging to a cookie object to destroy session's cookie
                 $this->report_error('Failed to delete session cookie with values : ' . json_encode($params));
                 return false;
             }
         }
+        $this->report_debug('Session destroyed');
         return true;
     }
 
@@ -44,10 +41,17 @@ class session extends mysfw\frame\dna{
     }
 
     public function get($k=null) {
+        $this->start();
         if($k===null) return $_SESSION;
         return @$_SESSION[$k];
     }
-    public function set($k, $v) {$_SESSION[$k]= $v; return $this;}
-    public function delete($k) {unset($_SESSION[$k]); return $this;}
+    public function set($k, $v) {
+        $this->start();
+        $_SESSION[$k]= $v; return $this;
+    }
+    public function delete($k){
+        $this->start();
+        unset($_SESSION[$k]); return $this;
+    }
 
  }
