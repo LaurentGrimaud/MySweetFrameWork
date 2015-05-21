2b3eea214f42cad58e355f6ba6d65b5bbab343d8
g4rmr <romain.m.giacalone@gmail.com>
2015-05-21 13:21:46 +0200

    updated interface

--- frame/contract/resource_iterator.php
+++ frame/contract/resource_iterator.php
@@ -3,5 +3,6 @@ namespace t0t1\mysfw\frame\contract;
 
 interface resource_iterator{
     public function wrap($resource,$operator_type);
-    public function count();
+    public function count($use_limit);
+    public function shift();
 }
