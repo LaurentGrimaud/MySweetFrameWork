20e8ffe5e8eb4b44aec2d02a52d6936289a196d1
Laurent Grimaud <laurent.grimaud@gmail.com>
2015-11-02 09:33:47 +0100

    URI management - Add complex configuration format

--- module/uri_builder/uri_builder.php
+++ module/uri_builder/uri_builder.php
@@ -1,9 +1,19 @@
 <?php
 
+ /**
+  * XXX Draft
+  *
+  * @missing configuration checks during get_ready()
+  */
+
  namespace t0t1\mysfw\module;
  use t0t1\mysfw\frame;
 
  class uri_builder extends frame\dna implements frame\contract\dna {
+  // rules => [ 'name' => RULE ]
+  // or
+  // rules => [ 'name' => ['rule' => RULE, 'ssst' => [ [...,...], ...] ] ]
+  
   protected $_defaults = [
    'uri_builder:rules' => []
   ];
@@ -20,7 +30,39 @@
     $this->report_error("No rule with`{$name}` found");
     throw $this->except();   // XXX
    }
-   $this->report_debug("Build URI using `{$rules[$name]}` with `".json_encode($args).'`');
-   return vsprintf($rules[$name], $args); // XXX draft
+   switch(gettype($rules[$name])) {
+    case 'string':
+     $this->report_debug("Found simple rule named `{$name}`");
+     return $this->_build($rules[$name], $args);
+
+    case 'array':
+    default:
+     $this->report_debug("Found complex rule named `{$name}`");
+     $rule = $rules[$name]['rule'];
+     $args = $this->_prepare_args($rules[$name]['ssst'], $args);
+     return $this->_build($rule, $args);
+   }
+  }
+
+  protected function _prepare_args($assistants, $args) {
+   $res = [];
+   for($i=0;$i < count($args); $i++) {
+    $res[$i] = $args[$i];
+    if(! @$assistants[$i]){
+     $this->report_debug("No assistant found for #`$i` argument `{$args[$i]}`");
+     continue;
+    }
+    foreach($assistants[$i] as $sst) {
+     $new = $sst($res[$i]);
+     $this->report_debug("Processed {$res[$i]} to $new");
+     $res[$i] = $new;
+    }
+   }
+   return $res;
+  }
+
+  protected function _build($rule, $args) {
+   $this->report_debug("Build URI using `{$rule}` with `".json_encode($args).'`');
+   return vsprintf($rule, $args); // XXX draft
   }
  }
