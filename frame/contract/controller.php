cf7ceee798069d5f6ca203a533010081b6aa98b8
Laurent Grimaud <laurent.grimaud@gmail.com>
2015-12-03 15:35:35 +0100

    Controller - Rationalisation

--- frame/contract/controller.php
+++ frame/contract/controller.php
@@ -2,7 +2,18 @@
  namespace t0t1\mysfw\frame\contract;
 
  interface controller {
+
+  // Pure controller logics
   public function control_and_reveal($p);
   public function control($p);
+
+  // Wrappers to view, to handle data to render
+  public function set($k, $v);
+  public function get($_);
+  public function set_all($_);
+
+  // A controller may be asked its view
+  // XXX really ?
+  public function get_view();
+
  }
-?>
--- module/generic_controller/generic_controller.php
+++ module/generic_controller/generic_controller.php
@@ -5,35 +5,19 @@
  use t0t1\mysfw\frame;
 
  $this->_learn('frame\contract\controller');
+ $this->_learn('module\controller_base\controller_base');
 
- class generic_controller extends frame\dna implements frame\contract\controller, frame\contract\dna {
+ class generic_controller extends controller_base implements frame\contract\controller, frame\contract\dna {
   private $_param;    // XXX
-  private $_v;        // Object implementing view contract
-  private $_tmpl;     // Template to be used by the view
   protected $_defaults = array(
     'generic_controller:view'        => 'http_response',      // Name of the view to be used by the controller
     'generic_controller:control_dir' => '../include/control/' // Directory to search for controls implementations
     );
 
-  protected function _set_tmpl($_){$this->_tmpl = $_;return $this;}
-  protected function _get_tmpl(){return $this->_tmpl;}
-
-  protected function _set($k, $v){$this->_v->set($k, $v);return $this;}
-  protected function _get($_){return $this->_v->get($_);}
-  
-  protected function _set_all($_){$this->_v->set_all($_);return $this;}
-
-  // XXX _get_ready() may be overriden by custom controllers
   protected function _get_ready() {
    $this->report_debug("Popping ".$this->inform('generic_controller:view'). " object");
-   $this->_v = $this->get_popper()->pop($this->inform('generic_controller:view'));
+   $this->_v = $this->pop($this->inform('generic_controller:view'));
   }
-  
-  // XXX external set should be forbidden
-  public function set($k, $v){return $this->_set($k, $v);}
-
-  public function set_param($param){$this->_param = $param;$this->_set_tmpl($param);return $this;}
-  public function get_view(){return $this->_v;}
 
   public function control_and_reveal($p) {
    $this->control($p);
@@ -41,10 +25,10 @@
   }
 
   public function control($p){
-   // XXX default implementation
    $this->report_debug("Will use control file `{$this->_param}`");
    $this->report_debug("Template set to `{$this->_get_tmpl()}`");
    include $this->inform('generic_controller:control_dir').$this->_param.'.php'; // XXX 
   }
 
+  public function set_param($param){$this->_param = $param;$this->_set_tmpl($param);return $this;}
  }
